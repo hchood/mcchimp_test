b6a2374984cb5bb0b4439e3f0c4158c99f0beae6
Helen Hood <helen.c.hood@gmail.com>
2014-01-06 13:48:13 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+McchimpTest::Application.config.secret_key_base = 'de2f72e11f1dacf98f22b77158f7688e18d08ccc691e3b14c5b120244435047ed4b6d636e6cda1fff4b6efe2f4ca28167528abfe5f9db2292c1460c933a0746a'
