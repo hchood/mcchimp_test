d804905b32cf0e4a2d0c48af79d48233df609c7f
Helen Hood <helen.c.hood@gmail.com>
2014-01-06 14:03:14 -0500

    Added mailchimp & set it up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
+require 'mailchimp'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :setup_mcapi
+
+  def setup_mcapi
+    @mc = Mailchimp::API.new('90f094ee3333a69af87f532c01d42f12-us3')
+    @list_id = '4c21c61086'
+  end
 end
